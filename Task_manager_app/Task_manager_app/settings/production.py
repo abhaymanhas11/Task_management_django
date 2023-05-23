fe83d50ca739680fc91ef62a2375516b803440f4
japjeet singh <designwithjapjeet@gmail.com>
2023-05-23 18:06:54 +0530

    test2

--- Task_manager_app/Task_manager_app/settings/production.py
+++ Task_manager_app/Task_manager_app/settings/production.py
@@ -9,14 +9,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-DEBUG = True
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
-}
+# DEBUG = True
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
